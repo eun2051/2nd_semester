cf2f0f0d0def9e146dca11a9b7f7f9a869afe474
eun2051 <genie5518@yu.ac.kr>
2025-10-13 13:19:17 +0900

    .

--- Structure/Week5/circularqueue.c
+++ Structure/Week5/circularqueue.c
@@ -1,15 +1,38 @@
-void addq(element item)
-{
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 100
+
+typedef struct {
+    int key;
+} element;
+element queue[MAX_QUEUE_SIZE];
+int front = -1;
+int rear = -1;
+
+bool IsFull() {
+    return (rear + 1) % MAX_QUEUE_SIZE == front;
+}
+
+bool IsEmpty() {
+    return front == rear;
+}
+
+void addq(element item) {
+    if (IsFull()) {
+        printf("원형 큐가 다 찼습니다.\n");
+        return ;
+    }
     rear = (rear + 1) % MAX_QUEUE_SIZE;
-    if (front == rear)
-        queueFull();
     queue[rear] = item;
 }
 
-element deleteq()
-{
-    if (front == rear)
-        return queueEmpty();
+element deleteq() {
+    if (IsEmpty()) {
+        printf("원형 큐가 비었습니다.\n");
+        exit(1);
+    }
     front = (front + 1) % MAX_QUEUE_SIZE;
     return queue[front];
 }
--- Structure/Week5/queue.c
+++ Structure/Week5/queue.c
@@ -1,13 +1,37 @@
-void addq(element item)
-{ /*queue에 item 삽입*/
-    if (rear == MAX_QUEUE_SIZE - 1)
-        queueFull();
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 100
+
+// 필수 구조체
+typedef struct {
+    int key;
+} element;
+element queue[MAX_QUEUE_SIZE];
+int front = -1;
+int rear = -1;
+
+bool IsFull() {
+    return rear == MAX_QUEUE_SIZE - 1;
+}
+
+bool IsEmpty() {
+    return front == rear;
+}
+
+void addq(element item) {
+    if (IsFull()) {
+        printf("큐가 가득 찼습니다.\n");
+        exit(1);
+    }
     queue[++rear] = item;
 }
 
-element deleteq()
-{ /*queue 앞에 있는 원소 삭제*/
-    if (front == rear)
-        return queueEmpty();
+element deleteq() {
+    if (IsEmpty()) {
+        printf("큐가 비었습니다.\n");
+        exit(1);
+    }
     return queue[++front];
 }
