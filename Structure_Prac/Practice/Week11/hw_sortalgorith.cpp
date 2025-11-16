3331c9d751ce92947eab0fe42aaea6155cfb433f
eun2051 <genie5518@naver.com>
2025-11-16 15:09:29 +0900

    Close

--- Structure_Prac/Practice/Week11/hw_sortalgorith.cpp
+++ Structure_Prac/Practice/Week11/hw_sortalgorith.cpp
@@ -23,8 +23,9 @@ void quickSort(element a[], int left, int right);
 
 int main(void)
 {
-    int i, n;
-    double times[3];
+    int n_value[] =  {1000, 10000, 100000, 1000000};
+    int n;
+    double times[6];
     element *A = (element *)malloc(sizeof(element) * MAX_SIZE);
     element *B = (element *)malloc(sizeof(element) * MAX_SIZE);
 
@@ -36,30 +37,40 @@ int main(void)
     }
     srand((unsigned int)time(NULL));
 
-    printf("정렬할 원소의 갯수 (n, 최대 %d)를 입력하세요 : ", MAX_SIZE - 1);
-    if (scanf("%d", &n) != 1 || n < 1 || n >= MAX_SIZE) {
-        fprintf(stderr, "부적절 n값\n");
-        free(A);
-        free(B);
-        return 1;
+    printf("12.1. 랜덤 생성 데이터의 정렬\n");
+    for (int j = 0; j < 4; j++) {
+        n = n_value[j];
+        for (int i = 1; i <= n; i++) {
+            A[i].key = ( (double)rand() / (RAND_MAX * 1.0) ) * 1000000000.0;
+        }
+        mesure_timesort(n, A, B, times);
+        print_array(n, times);
+        printf("\n");
     }
-
-    for (i = 1; i <= n; i++) {
-        A[i].key = ( (double)rand() / (RAND_MAX * 1.0) ) * 1000000000.0;
-    }
-
-    printf("12.1. 난수 생성 데이터의 정렬");
-    mesure_timesort(n, A, B, times);
-    print_array(n, times);
-    selectionSort(A, n);
     printf("12.2. 정렬된 데이터의 정렬");
-    mesure_timesort(n, A, B, times);
-    print_array(n, times);
-    printf("12.2.2 내림차순 정렬된 데이터의 정렬\n");
-    descending_sort(A, n);
-    mesure_timesort(n, A, B, times);
-    print_array(n, times);
-
+    printf("12.2.1. 오름차순 정렬된 데이터의 정렬\n");
+    for (int j = 0; j < 4; j++) {
+        n = n_value[j];
+        for (int i = 1; i <= n; i++) {
+            A[i].key = ( (double)rand() / (RAND_MAX * 1.0) ) * 1000000000.0;
+        }
+        selectionSort(A, n);
+        mesure_timesort(n, A, B, times);
+        print_array(n, times);
+        printf("\n");
+    }
+    printf("12.2.2. 내림차순 정렬된 데이터의 정렬\n");
+    for (int j = 0; j < 4; j++) {
+        n = n_value[j];
+        for (int i = 1; i <= n; i++) {
+            A[i].key = ( (double)rand() / (RAND_MAX * 1.0) ) * 1000000000.0;
+        }
+        selectionSort(A, n);
+        descending_sort(A, n);
+        mesure_timesort(n, A, B, times);
+        print_array(n, times);
+        printf("\n");
+    }
     free(A);
     free(B);
     return 0;
@@ -68,25 +79,44 @@ int main(void)
 void mesure_timesort(int n, element *A, element *B, double time_array[])
 {
     clock_t start, end;
+    int     success;
 
     copy_array(A, B, n);
     start = clock();
     selectionSort(B, n); 
     end = clock();
     time_array[0] = (double)(end - start) / CLOCKS_PER_SEC;
+    success = is_sorted(B, n);
+    time_array[1] = (double)success; 
+    if (!success) {
+        fprintf(stderr, "\nERROR: n=%d에서 Selection Sort가 실패했습니다! 프로그램 종료.\n", n);
+        exit(1); 
+    }
 
     copy_array(A, B, n);
     start = clock();
     insertionSort(B, n); 
     end = clock();
-    time_array[1] = (double)(end - start) / CLOCKS_PER_SEC;
+    time_array[2] = (double)(end - start) / CLOCKS_PER_SEC;
+    time_array[3] = (double)success;
+    if (!success) {
+        fprintf(stderr, "\nERROR: n=%d에서 Insertion Sort가 실패했습니다! 프로그램 종료.\n", n);
+        exit(1);
+    }
 
     copy_array(A, B, n);
     start = clock();
     quickSort(B, 1, n); 
     end = clock();
-    time_array[2] = (double)(end - start) / CLOCKS_PER_SEC;
+    time_array[4] = (double)(end - start) / CLOCKS_PER_SEC;
+    success = is_sorted(B, n);
+    time_array[5] = (double)success;
+    if (!success) {
+        fprintf(stderr, "\nERROR: n=%d에서 Quick Sort가 실패했습니다! 프로그램 종료.\n", n);
+        exit(1);
+    }
 }
+
 void descending_sort(element a[], int n) {
     element tmp;
     for (int i = 1; i <= n / 2; i++) {
@@ -107,8 +137,8 @@ void print_array(int n, double times[])
 {
     printf(" 원소 수 : %d\n", n);
     printf(" selection sort time: %.6f\n", times[0]);
-    printf(" insertion sort time: %.6f\n", times[1]);
-    printf("       quick sort time: %.6f\n", times[2]);
+    printf(" insertion sort time: %.6f\n", times[2]);
+    printf("     quick sort time: %.6f\n", times[4]);
 }
 
 void copy_array(element src[], element dest[], int n)
